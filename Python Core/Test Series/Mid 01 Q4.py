2a3b236264daa1602ff947fd2ffc55e95d0ffcca
Suryaa-k <suryaanish1149198@gmail.com>
2025-12-05 16:03:55 +0530

    Update Files

--- Python Core/Test Series/Mid 01 Q4.py
+++
@@ -1,3 +0,0 @@
-# 4.	Create a base class Transport with move() and derived classes Bus and Bike that override it but also call the parent implementation using super().
-# Show the combination of reuse + custom behavior.
-
