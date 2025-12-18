96f951c40bcab2c92ef0a116addbe06c013a7187
Suryaa-k <suryaanish1149198@gmail.com>
2025-12-18 14:34:57 +0530

    Update Files

---
+++ Python Core/Error Handling/EH 01/Q1.py
@@ -0,0 +1,4 @@
+# 1. Write a Python program that attempts to dynamically import a module at 
+# runtime. The program should only import the module if it actually exists; 
+# otherwise, it should print "Module does not exist".
+
---
+++ Python Core/Error Handling/EH 02/Class Person.py
@@ -0,0 +1,3 @@
+# • Create a class Person whose constructor takes age as an argument. Raise a 
+# ValueError if the age is less than 0.
+
