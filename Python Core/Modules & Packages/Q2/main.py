ec9696b9f188f92fb6e3f3ecd45a5eb41842e4d7
Suryaa-k <suryaanish1149198@gmail.com>
2025-12-27 15:20:06 +0530

    Update Files

--- Python Core/Modules & Packages/Q2/main.py
+++ Python Core/Modules & Packages/Q2/main.py
@@ -16,10 +16,11 @@ parent_rect = Rectangle(4, 6)
 # Create child class object
 child_circle = ColoredCircle(7, "red")
 
-# Test them
+#Test them
 print(parent_circle)        # Circle(radius=5)
 print(f"Circle area: {parent_circle.area():.2f}")
 print(parent_rect)          # Rectangle(4x6)
 print(f"Rect area: {parent_rect.area()}") 
 print(child_circle)         # ColoredCircle(radius=7, color='red')
 print(f"Colored circle area: {child_circle.area():.2f}")
+
