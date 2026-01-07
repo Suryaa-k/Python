e8eb0eeff473168361d167dff5f65ec1cec52d4e
Suryaa-k <suryaanish1149198@gmail.com>
2026-01-07 15:59:09 +0530

    Update Files

---
+++ Python Programming/DSA/Lists/main.py
@@ -0,0 +1,33 @@
+# l = list(map(int, input().split()))
+# l.append
+# l.clear
+# l.copy
+# l.count
+# l.extend
+# l.index
+# l.insert
+# l.pop
+# l.remove
+# l.reverse
+# l.sort
+
+# l=[10, 20, 30, 40, 20, 20, 30, 40]
+# mre=0
+# maxc=0
+# for i in range(len(l)):
+#     oc=0
+#     for j in range(len(l)):
+#         if l[i]==l[j]:
+#             oc+=1
+#     if oc > maxc:
+#         maxc = oc
+#         mre = [l[i]]
+#     elif oc == maxc:
+#         if l[i] not in mre:
+#             mre.append(l[i])
+# print(f"Most frequent number(s): {mre}")
+
+
+
+
+
