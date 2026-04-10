301ca420f3698c02453a3de358badee28d0fc2ec
Suryaa-k <suryaanish1149198@gmail.com>
2026-04-10 15:24:11 +0530

    Sets & Dictionaries

---
+++ Python Core/Numpy & Pandas/Image Brightness Adjustment.py
@@ -0,0 +1,3 @@
+# You are given a grayscale image represented as a 2D NumPy array.
+# Increase the brightness of the image by adding a constant value (e.g., +50)
+# using broadcasting. Ensure pixel values do not exceed 255.
